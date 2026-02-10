cdea4099d226a58c7d05bfede6d4d7a4c0180f46
Денис Лабутин <denislabutin@MacBook-Pro-Denis.local>
2026-02-11 02:26:55 +0300

    test blurred header

--- src/features/AppHeader/AppHeader.tsx
+++ src/features/AppHeader/AppHeader.tsx
@@ -8,17 +8,20 @@ type Props = {
 
 const AppHeader: React.FC<Props> = ({ align = "center" }) => {
   return (
-    <header className={`${styles.header} ${styles[align]}`}>
-      <img src={logo} className={styles.logo} alt="logo" />
-      <div className={styles.gradientBlur}>
+    <div className={styles.container}>
+      <div className={styles.placeholder}></div>
+      <header className={`${styles.header} ${styles[align]}`}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <div className={styles.shadowGradientBlur}>
+          {/* <div></div>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </header>
+        <div></div> */}
+        </div>
+      </header>
+    </div>
   );
 };
 
